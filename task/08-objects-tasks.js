d04a80e23ed2faa922ad965808d76e08ca492bdb
Mark-programmer <markhel_al@mail.ru>
2026-05-30 16:23:00 +0300

    Complete 08 task

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -2,45 +2,36 @@
 
 /**************************************************************************************************
  *                                                                                                *
- * Plese read the following tutorial before implementing tasks:                                   *
+ * Please read the following tutorial before implementing tasks:                                   *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object        *
  *                                                                                                *
  **************************************************************************************************/
 
-
 /**
- * Returns the rectagle object with width and height parameters and getArea() method
+ * Returns the rectangle object with width and height parameters and getArea() method
  *
  * @param {number} width
  * @param {number} height
  * @return {Object}
- *
- * @example
- *    var r = new Rectangle(10,20);
- *    console.log(r.width);       // => 10
- *    console.log(r.height);      // => 20
- *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+    this.width = width;
+    this.height = height;
 }
-
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+};
 
 /**
  * Returns the JSON representation of specified object
  *
  * @param {object} obj
  * @return {string}
- *
- * @example
- *    [1,2,3]   =>  '[1,2,3]'
- *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    return JSON.stringify(obj);
 }
-
 
 /**
  * Returns the object of specified type from JSON representation
@@ -48,15 +39,13 @@ function getJSON(obj) {
  * @param {Object} proto
  * @param {string} json
  * @return {object}
- *
- * @example
- *    var r = fromJSON(Rectangle.prototype, '{"width":10, "height":20}');
- *
  */
 function fromJSON(proto, json) {
-    throw new Error('Not implemented');
+    const obj = Object.create(proto);
+    const data = JSON.parse(json);
+    Object.assign(obj, data);
+    return obj;
 }
-
 
 /**
  * Css selectors builder
@@ -71,7 +60,7 @@ function fromJSON(proto, json) {
  *
  * The task is to design a single class, independent classes or classes hierarchy and implement the functionality
  * to build the css selectors using the provided cssSelectorBuilder.
- * Each selector should have the stringify() method to output the string repsentation according to css specification.
+ * Each selector should have the stringify() method to output the string representation according to css specification.
  *
  * Provided cssSelectorBuilder should be used as facade only to create your own classes,
  * for example the first method of cssSelectorBuilder can be like this:
@@ -80,63 +69,120 @@ function fromJSON(proto, json) {
  *   },
  *
  * The design of class(es) is totally up to you, but try to make it as simple, clear and readable as possible.
- *
- * @example
- *
- *  var builder = cssSelectorBuilder;
- *
- *  builder.id('main').class('container').class('editable').stringify()  => '#main.container.editable'
- *
- *  builder.element('a').attr('href$=".png"').pseudoClass('focus').stringify()  => 'a[href$=".png"]:focus'
- *
- *  builder.combine(
- *      builder.element('div').id('main').class('container').class('draggable'),
- *      '+',
- *      builder.combine(
- *          builder.element('table').id('data'),
- *          '~',
- *           builder.combine(
- *               builder.element('tr').pseudoClass('nth-of-type(even)'),
- *               ' ',
- *               builder.element('td').pseudoClass('nth-of-type(even)')
- *           )
- *      )
- *  ).stringify()        =>    'div#main.container.draggable + table#data ~ tr:nth-of-type(even)   td:nth-of-type(even)'
- *
- *  For more examples see unit tests.
  */
 
+class CssSelector {
+    constructor(parts = []) {
+        this.parts = parts; // each part: { type, value }
+    }
+
+    _getOrderIndex(type) {
+        const order = {
+            element: 0,
+            id: 1,
+            class: 2,
+            attribute: 3,
+            pseudoClass: 4,
+            pseudoElement: 5
+        };
+        return order[type];
+    }
+
+    _addPart(type, value, orderIndex) {
+        // Check for single occurrence types
+        const singleTypes = ['element', 'id', 'pseudoElement'];
+        if (singleTypes.includes(type) && this.parts.some(p => p.type === type)) {
+            throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+        }
+
+        // Check order
+        const lastOrder = this.parts.length ? this._getOrderIndex(this.parts[this.parts.length - 1].type) : -1;
+        if (orderIndex < lastOrder) {
+            throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+        }
+
+        return new CssSelector([...this.parts, { type, value }]);
+    }
+
+    element(value) {
+        return this._addPart('element', value, 0);
+    }
+
+    id(value) {
+        return this._addPart('id', value, 1);
+    }
+
+    class(value) {
+        return this._addPart('class', value, 2);
+    }
+
+    attr(value) {
+        return this._addPart('attribute', value, 3);
+    }
+
+    pseudoClass(value) {
+        return this._addPart('pseudoClass', value, 4);
+    }
+
+    pseudoElement(value) {
+        return this._addPart('pseudoElement', value, 5);
+    }
+
+    stringify() {
+        return this.parts.map(part => {
+            switch (part.type) {
+                case 'element': return part.value;
+                case 'id': return '#' + part.value;
+                case 'class': return '.' + part.value;
+                case 'attribute': return '[' + part.value + ']';
+                case 'pseudoClass': return ':' + part.value;
+                case 'pseudoElement': return '::' + part.value;
+            }
+        }).join('');
+    }
+}
+
+class CombinedSelector {
+    constructor(selector1, combinator, selector2) {
+        this.selector1 = selector1;
+        this.combinator = combinator;
+        this.selector2 = selector2;
+    }
+
+    stringify() {
+        const left = this.selector1.stringify();
+        const right = this.selector2.stringify();
+        if (this.combinator === ' ') {
+            return left + '   ' + right;
+        } else {
+            return left + ' ' + this.combinator + ' ' + right;
+        }
+    }
+}
+
 const cssSelectorBuilder = {
-
     element: function(value) {
-        throw new Error('Not implemented');
+        return new CssSelector().element(value);
     },
-
     id: function(value) {
-        throw new Error('Not implemented');
+        return new CssSelector().id(value);
     },
-
     class: function(value) {
-        throw new Error('Not implemented');
+        return new CssSelector().class(value);
     },
-
     attr: function(value) {
-        throw new Error('Not implemented');
+        return new CssSelector().attr(value);
     },
-
     pseudoClass: function(value) {
-        throw new Error('Not implemented');
+        return new CssSelector().pseudoClass(value);
     },
-
     pseudoElement: function(value) {
-        throw new Error('Not implemented');
+        return new CssSelector().pseudoElement(value);
     },
-
     combine: function(selector1, combinator, selector2) {
-        throw new Error('Not implemented');
-    },
+        return new CombinedSelector(selector1, combinator, selector2);
+    }
 };
-
 
 module.exports = {
     Rectangle: Rectangle,
