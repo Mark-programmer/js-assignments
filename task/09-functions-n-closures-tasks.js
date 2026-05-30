d20506dd4419c64911bf5a9bf417897305faf063
Mark-programmer <markhel_al@mail.ru>
2026-05-30 16:23:10 +0300

    Complete 09 task

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -2,14 +2,13 @@
 
 /**********************************************************************************************
  *                                                                                            *
- * Plese read the following tutorial before implementing tasks:                               *
+ * Please read the following tutorial before implementing tasks:                               *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions                    *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function  *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments      *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures                           *
  *                                                                                            *
  **********************************************************************************************/
-
 
 /**
  * Returns the functions composition of two specified functions f(x) and g(x).
@@ -20,36 +19,24 @@
  * @param {Function} f
  * @param {Function} g
  * @return {Function}
- *
- * @example
- *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.acos(x))
- *
  */
-function getComposition(f,g) {
-    throw new Error('Not implemented');
+function getComposition(f, g) {
+    return function(x) {
+        return f(g(x));
+    };
 }
-
 
 /**
  * Returns the math power function with the specified exponent
  *
  * @param {number} exponent
  * @return {Function}
- *
- * @example
- *   var power2 = getPowerFunction(2); // => x^2
- *   power2(2) => 4
- *   power2(4) => 16
- *
- *   var power05 = getPowerFunction(0.5); // => x^0.5
- *   power05(4) => 2
- *   power05(16) => 4
- *
  */
 function getPowerFunction(exponent) {
-    throw new Error('Not implemented');
+    return function(x) {
+        return Math.pow(x, exponent);
+    };
 }
-
 
 /**
  * Returns the polynom function of one argument based on specified coefficients.
@@ -57,36 +44,40 @@ function getPowerFunction(exponent) {
  *
  * @params {integer}
  * @return {Function}
- *
- * @example
- *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
- *   getPolynom(1,-3)  => y = x - 3
- *   getPolynom(8)     => y = 8
- *   getPolynom()      => null
  */
 function getPolynom() {
-    throw new Error('Not implemented');
+    const coefficients = Array.from(arguments);
+    if (coefficients.length === 0) {
+        return null;
+    }
+    return function(x) {
+        let result = 0;
+        const n = coefficients.length;
+        for (let i = 0; i < n; i++) {
+            result += coefficients[i] * Math.pow(x, n - i - 1);
+        }
+        return result;
+    };
 }
-
 
 /**
  * Memoizes passed function and returns function
  * which invoked first time calls the passed function and then always returns cached result.
  *
- * @params {Function} func - function to memoize
+ * @param {Function} func - function to memoize
  * @return {Function} memoized function
- *
- * @example
- *   var memoizer = memoize(() => Math.random());
- *   memoizer() => some random number  (first run, evaluates the result of Math.random())
- *   memoizer() => the same random number  (second run, returns the previous cached result)
- *   ...
- *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
 function memoize(func) {
-    throw new Error('Not implemented');
+    let cached = null;
+    let called = false;
+    return function() {
+        if (!called) {
+            cached = func();
+            called = true;
+        }
+        return cached;
+    };
 }
-
 
 /**
  * Returns the function trying to call the passed function and if it throws,
@@ -95,85 +86,70 @@ function memoize(func) {
  * @param {Function} func
  * @param {number} attempts
  * @return {Function}
- *
- * @example
- * var attempt = 0, retryer = retry(() => {
- *      if (++attempt % 2) throw new Error('test');
- *      else return attempt;
- * }, 2);
- * retryer() => 2
  */
 function retry(func, attempts) {
-    throw new Error('Not implemented');
+    return function() {
+        let lastError;
+        for (let i = 0; i < attempts; i++) {
+            try {
+                return func();
+            } catch (err) {
+                lastError = err;
+            }
+        }
+        throw lastError;
+    };
 }
-
 
 /**
  * Returns the logging wrapper for the specified method,
  * Logger has to log the start and end of calling the specified function.
  * Logger has to log the arguments of invoked function.
- * The fromat of output log is:
+ * The format of output log is:
  * <function name>(<arg1>, <arg2>,...,<argN>) starts
  * <function name>(<arg1>, <arg2>,...,<argN>) ends
- *
  *
  * @param {Function} func
  * @param {Function} logFunc - function to output log with single string argument
  * @return {Function}
- *
- * @example
- *
- * var cosLogger = logger(Math.cos, console.log);
- * var result = cosLogger(Math.PI));     // -1
- *
- * log from console.log:
- * cos(3.141592653589793) starts
- * cos(3.141592653589793) ends
- *
  */
 function logger(func, logFunc) {
-    throw new Error('Not implemented');
+    return function(...args) {
+        const argsString = args.map(arg => JSON.stringify(arg)).join(',');
+        const startMsg = `${func.name}(${argsString}) starts`;
+        logFunc(startMsg);
+        const result = func(...args);
+        const endMsg = `${func.name}(${argsString}) ends`;
+        logFunc(endMsg);
+        return result;
+    };
 }
-
 
 /**
  * Return the function with partial applied arguments
  *
  * @param {Function} fn
  * @return {Function}
- *
- * @example
- *   var fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
- *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn) {
-    throw new Error('Not implemented');
+    const fixedArgs = Array.from(arguments).slice(1);
+    return function(...restArgs) {
+        return fn(...fixedArgs, ...restArgs);
+    };
 }
-
 
 /**
  * Returns the id generator function that returns next integer starting from specified number every time when invoking.
  *
  * @param {Number} startFrom
  * @return {Function}
- *
- * @example
- *   var getId4 = getIdGenerator(4);
- *   var getId10 = gerIdGenerator(10);
- *   getId4() => 4
- *   getId10() => 10
- *   getId4() => 5
- *   getId4() => 6
- *   getId4() => 7
- *   getId10() => 11
  */
 function getIdGeneratorFunction(startFrom) {
-    throw new Error('Not implemented');
+    let current = startFrom;
+    return function() {
+        return current++;
+    };
 }
-
 
 module.exports = {
     getComposition: getComposition,
